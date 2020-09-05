42b7b2afee12fa86d95f3bd00edb153a15dc1679
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-05 08:12:05 -0700

    :tada: Create constants

---
+++ src/constants/icons.js
@@ -0,0 +1,12 @@
+import MORE_ICON from "../assets/icons/more_24.png"
+import LOVE_ICON from "../assets/icons/love_24.png"
+import LOVE_ICON_FILLED from "../assets/icons/love_filled_24.png"
+import COMMENT_ICON from "../assets/icons/comment_24.png"
+import SHARE_ICON from "../assets/icons/share_24.png"
+export {
+    MORE_ICON,
+    LOVE_ICON,
+    LOVE_ICON_FILLED,
+    COMMENT_ICON,
+    SHARE_ICON,
+}
---
+++ src/constants/paths.js
@@ -0,0 +1,4 @@
+import DEFAULT_USER_PHOTO_URL from "../assets/img/avatar.jpg"
+import DEFAULT_POST_PHOTO_URL from "../assets/img/post.jpg"
+
+export { DEFAULT_USER_PHOTO_URL, DEFAULT_POST_PHOTO_URL }
