4e1c877b789ad647160a627e9fbb7e509b029f91
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-05 08:09:44 -0700

    :tada: Implement App component

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Card from "../components/Card"
+
+const App = () => (
+    <div className="flex flex-col items-center align-baseline justify-center mt-24">
+        <Card />
+    </div>
+)
+
+export default App
