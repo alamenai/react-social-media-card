3a99e1e80e4b7c0cca59d419fd3b2aea16b264a4
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-05 08:47:54 -0700

    :tada: Create footer layout

---
+++ src/components/Card/Footer/Layout.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Layout = ({ children }) => (
+    <div className="flex bg-white mt-4 justify-around w-full">
+        {children}
+    </div>)
+
+export default Layout
