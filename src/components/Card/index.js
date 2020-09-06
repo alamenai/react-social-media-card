d805d2a01c2d8fe6a3c7f5be91802e9a2a4fee67
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-06 02:54:43 -0700

    :cyclone: Wrap the Card with the OverlayProvider

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "./Layout"
 import Header from "./Header"
 import Body from "./Body"
 import Footer from "./Footer"
+import Overlay from "./Overlay"
+import Actions from "./Actions"
+import { Context as OverlayContext } from "./Overlay/context"
 
 const Card = () => {
-    return <div className="w-full flex justify-center">
-        <Layout>
-            <Header />
-            <Body />
-            <Footer />
-        </Layout>
-    </div>
+
+    const [visibility, setVisibility] = useState(false)
+    const value = { visibility, setVisibility }
+
+    return <OverlayContext.Provider value={value}>
+        <div className="w-full flex justify-center">
+            <Layout>
+                <Header />
+                <Body />
+                <Footer />
+                <Overlay>
+                    <Actions />
+                </Overlay>
+            </Layout>
+        </div>
+    </OverlayContext.Provider>
 }
 
 export default Card
