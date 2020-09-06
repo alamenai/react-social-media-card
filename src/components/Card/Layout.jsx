8ca210990f6502f2a3dd863643a1873981607b11
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-06 02:57:19 -0700

    :zap: Add Overlay component

--- src/components/Card/Layout.jsx
+++ src/components/Card/Layout.jsx
@@ -1,11 +1,9 @@
 import React from "react"
-import Overlay from "./Overlay"
 
-const Layout = (props) => {
+const Layout = ({children}) => {
     return (<div className="xs:w-11/12 xs:mx-4 sm:w-3/4 sm:mx-3 md:w-1/2 md:mx-3
                             bg-white w-1/3 rounded-xl h-auto shadow-xl py-4 relative">
-        <Overlay />
-        {props.children}
+        {children}
     </div>)
 }
 
