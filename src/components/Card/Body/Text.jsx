cf08295f897f42515e39d90420ec513badda7803
MenaiAla <ed.sheeran.eng@gmail.com>
2020-09-06 04:53:54 -0700

    :zap: Replace hashtag word with a link

--- src/components/Card/Body/Text.jsx
+++ src/components/Card/Body/Text.jsx
@@ -3,7 +3,13 @@ import PropTypes from "prop-types"
 
 const Text = ({ content }) => (
     <div className="mt-2 mb-4 mx-4">
-        <p className="text-sm text-justify">{content}</p>
+        <p className="text-sm text-justify">
+            {content.split(" ").map((str) => {
+                if (str.startsWith("#")) {
+                    return <a href={`/${str}`} className="text-blue-500">{str} </a>;
+                }
+                return str + " ";
+            })}</p>
     </div>
 )
 
